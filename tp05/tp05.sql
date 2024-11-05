c9a0f83060f91339240194f876cccee4527a822a
flo <furnari.florian@hotmail.fr>
2024-11-05 15:05:39 +0100

    correction tp05

--- tp05/tp05.sql
+++ tp05/tp05.sql
@@ -20,6 +20,12 @@ WHERE NOT EXISTS (
     SELECT 1 FROM COMPO WHERE COMPO.ID_ART = ARTICLE.ID
 );
 
+UPDATE article
+LEFT JOIN compo ON article.ID = compo.ID_ART
+SET PRIX = PRIX*0.9
+WHERE ID_ART IS NULL;
+ 
+
 -- d.	Une erreur s’est glissée dans les commandes concernant Française d’imports. Les chiffres en base ne sont pas bons. Il faut doubler les quantités de tous les articles commandés à cette société.
 
 UPDATE COMPO 
@@ -29,4 +35,11 @@ SET COMPO.QTE = COMPO.QTE * 2
 WHERE FOURNISSEUR.NOM = 'Française d''Imports';
 
 -- e.	Mettez au point une requête update qui permette de supprimer les éléments entre parenthèses dans les désignations. Il vous faudra utiliser des fonctions comme substring et position.
-
+INSERT INTO ARTICLE (ID, REF, DESIGNATION, PRIX, ID_FOU)
+VALUES
+(12, 'A01', 'Perceuse P1 (très partique) la perceuse', 74.99, 1);
+ 
+UPDATE article
+SET designation = concat(substring(designation, 1, position('(' IN designation)-1),
+                        substring(designation, position(')' IN designation)+2))
+WHERE designation LIKE '%(%)%';
